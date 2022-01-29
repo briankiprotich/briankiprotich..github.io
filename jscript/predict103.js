fcc6e2094169bb0dab15b3859b9e5c869c874ca9
briankiprotich <33713471+briankiprotich@users.noreply.github.com>
2022-01-29 17:01:38 +0300

    Update predict103.js

--- jscript/predict103.js
+++ jscript/predict103.js
@@ -45,8 +45,8 @@ $("#image-selector").change(function () {
 let model;
 (async function () {
 	
-	model = await tf.loadModel('https://github.com/briankiprotich/briankiprotich.github.io/blob/main/model_tb_1/model.json');
-	$("#selected-image").attr("src", "https://github.com/briankiprotich/briankiprotich.github.io/blob/main/assets/tb.jpg")
+	model = await tf.loadModel('https://github.com/briankiprotich/briankiprotich.github.io/model_tb_1/model.json');
+	$("#selected-image").attr("src", "https://github.com/briankiprotich/briankiprotich.github.io/assets/tb.jpg")
 	
 	
 	
